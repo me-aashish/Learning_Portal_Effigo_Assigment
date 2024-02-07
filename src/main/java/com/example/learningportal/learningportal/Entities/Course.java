ee680c9c5d1026e2acf4ba7daf8deaf06be057f8
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-02-07 10:49:01 +0530

    configured course and favourite entity

--- src/main/java/com/example/learningportal/learningportal/Entities/Course.java
+++ src/main/java/com/example/learningportal/learningportal/Entities/Course.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -46,4 +47,6 @@ public class Course {
 	@JsonIgnore
 	List<Enrollment> enrollments;
 
+	@ManyToMany(mappedBy = "courses", cascade = CascadeType.ALL)
+	List<Favourite> favourites;
 }
--- src/main/java/com/example/learningportal/learningportal/Entities/Favourite.java
+++ src/main/java/com/example/learningportal/learningportal/Entities/Favourite.java
@@ -1,10 +1,13 @@
 package com.example.learningportal.learningportal.Entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -25,4 +28,7 @@ public class Favourite {
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	@ManyToMany
+	List<Course> courses;
 }
